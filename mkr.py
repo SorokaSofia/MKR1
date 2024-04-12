0d82db42ca846bbf0ea75ccd8c88032d99c62850
SorokaSofia <s.soroka31@knu.ua>
2024-04-12 17:17:01 +0300

    Define function to calculate price change

--- mkr.py
+++ mkr.py
@@ -4,3 +4,10 @@ from datetime import datetime, timedelta
 # Function for reading data from a file
 def read_data(filename):
     return pd.read_csv(filename, names=['product_name', 'date', 'price'], parse_dates=['date'])
+
+# A function to determine the price change for the last month for a specific product
+def price_change_last_month(data, product_name):
+    today = datetime.today()
+    one_month_ago = today - timedelta(days=30)
+    filtered_data = data[(data['date'] >= one_month_ago) & (data['product_name'] == product_name)]
+    return filtered_data
