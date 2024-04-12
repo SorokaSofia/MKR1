089192751141e60e62a2640d327a04b3b5433b06
SorokaSofia <s.soroka31@knu.ua>
2024-04-12 17:16:30 +0300

    Define function to read data from CSV file

--- mkr.py
+++ mkr.py
@@ -1,2 +1,6 @@
 import pandas as pd
 from datetime import datetime, timedelta
+
+# Function for reading data from a file
+def read_data(filename):
+    return pd.read_csv(filename, names=['product_name', 'date', 'price'], parse_dates=['date'])
